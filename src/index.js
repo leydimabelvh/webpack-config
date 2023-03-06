f3361c962f5a4b401e58ed39d25a077c983a5220
Leydi Velazco <leydi.velazco@outlook.es>
2023-03-06 16:31:28 -0500

    Add test function

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { getGreeting } from './js/get-greeting';
+
+getGreeting( 'Mabel' );
---
+++ src/js/get-greeting.js
@@ -0,0 +1,7 @@
+
+export const getGreeting = ( name ) => {
+    const h2 = document.createElement( 'h2' );
+    h2.innerText = ` Hello, ${ name } `;
+    
+    document.body.append( h2 );
+}
